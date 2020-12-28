58ee6d68d52e4f418d20989cf13732d2e337eb9b
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2020-12-28 19:22:10 +0100

    Fixes
    There is a mistake in the blog article and this file.

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./prod/deploy.yml --private-key=./ssh_keys104.236.66.172_prod_key -K -u deployer -i ./prod/hosts -vvv
+ansible-playbook ./prod/deploy.yml --private-key=./ssh-keys/104.236.66.172_prod_key -K -u deployer -i ./prod/hosts -vvv
